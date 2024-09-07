c6e3d33f1d94ce30a93f40f6279d290fd9a554a1
Felipe Souza <fn23886@gmail.com>
2024-09-07 15:54:04 -0300

    feat darkmode

---
+++ darkmode.js
@@ -0,0 +1,21 @@
+
+  const toggleSwitch = document.querySelector('.input');
+  const currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+    }
+  }
+
+  toggleSwitch.addEventListener('change', function () {
+    if (this.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  });
+
